9f7e4307d17bb8d6e7879343c5ec85d545c56697
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-06 22:19:29 +0700

    Experiment 3.2: Be Creative!

--- README.md
+++ README.md
@@ -4,7 +4,13 @@
 
 ## 3.1. Original code
 
-![alt text](image.png)
-![alt text](image-1.png)
-![alt text](image-2.png)
-![alt text](image-3.png)
+![Insert username](image.png)
+![chatting](image-1.png)
+![yewchat running](image-2.png)
+![websocket running](image-3.png)
+
+## 3.2. Add some creativities to the webclient
+
+![New chatting theme](image-4.png)
+
+Di sini saya melakukan perubahan terhadap penggunaan warna dari chat page menjadi bertema hijau dan saya menggunakan avatar yang berbeda dengan melampirkan link ke gambar avatar tersebut. Selain itu saya juga menambahkan efek hover pada beberapa bagian tertentu seperti user, chat, dan tombol untuk mengirim pesan.
--- YewChat/src/components/chat.rs
+++ YewChat/src/components/chat.rs
@@ -93,8 +93,7 @@ impl Component for Chat {
                             .map(|u| UserProfile {
                                 name: u.into(),
                                 avatar: format!(
-                                    "https://avatars.dicebear.com/api/adventurer-neutral/{}.svg",
-                                    u
+                                    "https://static.vecteezy.com/system/resources/previews/022/506/362/original/simple-man-flat-design-avatar-free-vector.jpg"
                                 )
                                 .into(),
                             })
@@ -140,12 +139,12 @@ impl Component for Chat {
 
         html! {
             <div class="flex w-screen">
-                <div class="flex-none w-56 h-screen bg-gray-100">
-                    <div class="text-xl p-3">{"Users"}</div>
+                <div class="flex-none w-56 h-screen bg-green-200">
+                    <div class="text-xl p-3 text-white bg-green-500">{"Users"}</div>
                     {
                         self.users.clone().iter().map(|u| {
                             html!{
-                                <div class="flex m-3 bg-white rounded-lg p-2">
+                                <div class="flex m-3 bg-white rounded-lg p-2 hover:bg-gray-100 transition duration-300">
                                     <div>
                                         <img class="w-12 h-12 rounded-full" src={u.avatar.clone()} alt="avatar"/>
                                     </div>
@@ -162,14 +161,14 @@ impl Component for Chat {
                         }).collect::<Html>()
                     }
                 </div>
-                <div class="grow h-screen flex flex-col">
-                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
+                <div class="grow h-screen flex flex-col bg-gray-100">
+                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3 text-white bg-green-500">{"💬 Chat!"}</div></div>
                     <div class="w-full grow overflow-auto border-b-2 border-gray-300">
                         {
                             self.messages.iter().map(|m| {
                                 let user = self.users.iter().find(|u| u.name == m.from).unwrap();
                                 html!{
-                                    <div class="flex items-end w-3/6 bg-gray-100 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg ">
+                                    <div class="flex items-end w-3/6 bg-gray-200 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg hover:bg-gray-300 transition duration-300">
                                         <img class="w-8 h-8 rounded-full m-3" src={user.avatar.clone()} alt="avatar"/>
                                         <div class="p-3">
                                             <div class="text-sm">
@@ -187,11 +186,11 @@ impl Component for Chat {
                                 }
                             }).collect::<Html>()
                         }
-
+            
                     </div>
                     <div class="w-full h-14 flex px-3 items-center">
-                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                        <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
+                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-200 rounded-full outline-none focus:text-gray-700" name="message" required=true />
+                        <button onclick={submit} class="p-3 shadow-sm bg-green-600 w-10 h-10 rounded-full flex justify-center items-center text-white hover:bg-green-700 transition duration-300"> 
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                             </svg>
@@ -200,5 +199,6 @@ impl Component for Chat {
                 </div>
             </div>
         }
+        
     }
 }
